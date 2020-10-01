7cbf4887ad8209e8a17cf417446b02c986447215
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-10-01 10:00:36 +0530

    Update main.js
    Added Turnery operater

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ var button=document.querySelector("button");
 var filled=false;
 button.addEventListener("click",()=>{
 	//alert('connected');
-	if(filled)
-		document.body.style.background="blue";
-	else
-		document.body.style.background="green";
+	filled ? document.body.style.background="blue" : document.body.style.background="green"
 	filled=!filled;
 });
 
